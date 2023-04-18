f0777f82f2a662a1a10dfaa672e23cd1a98acac8
hayaboy <hayaboy@korea.ac.kr>
2023-04-18 12:12:24 +0900

    4/18

--- src/main/java/com/myboot01/DemoController.java
+++ src/main/java/com/myboot01/DemoController.java
@@ -1,6 +1,7 @@
 package com.myboot01;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,4 +14,40 @@ public class DemoController {
 		System.out.println("nice");
 		return "hi";
 	}
+	
+	
+	
+	
+	@RequestMapping("/kkk.do")
+	public String fdsrwefgr(Model model) {
+		
+		model.addAttribute("futggyygugkh", "ㅋㅋㅋ");
+		
+		return "hello2";
+	}
+	
+	
+	@RequestMapping("/hihi.do")
+	public String fsd() {
+		return "hello3";
+	}
+	
+	  @RequestMapping("/hello.do")
+	  public String hello(Model model){
+		System.out.println("안녕하세요");
+		model.addAttribute("message","hello.jsp입니다.!");
+		return "hello";
+	  } 
+	
+	  
+	  
+	  
+	  @RequestMapping("/hi.do")
+	  public String hi(Model model){
+		System.out.println("안녕하세요");
+		model.addAttribute("message","hi.jsp입니다.!");
+		return "hi";
+	  } 
+	  
+	  
 }
---
+++ src/main/resources/static/js/scriptTest.js
@@ -0,0 +1,4 @@
+function test(){
+	
+	alert("타임리프 테스트");
+}
